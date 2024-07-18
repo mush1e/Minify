b4ae224c9d90dd5022c0f82ecdee6cab94de5a10
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2024-07-18 01:51:16 -0400

    task/ arrays and slices

--- main.go
+++ main.go
@@ -1,28 +1,12 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
-
-func division(numerator, denominator int) (result, remainder int, err error) {
-	if denominator == 0 {
-		err = errors.New("Cannot divide by 0")
-		return 0, 0, err
-	}
-	result = numerator / denominator
-	remainder = numerator % denominator
-	return
-}
+import "fmt"
 
 func main() {
-	defer fmt.Println("Hello world!")
-	result, remainder, err := division(12, 3)
-	if err != nil {
-		fmt.Println(err)
-	} else if remainder == 0 {
-		fmt.Println("Perfect division")
-	} else {
-		fmt.Println(result, remainder, err)
-	}
+	intArr := [5]int32{}
+	intArr[0] = 123
+	fmt.Println(intArr[:])
+	intSlice := []int32{1, 2}
+	intSlice = append(intSlice, intArr[:]...)
+	fmt.Println(intSlice)
 }
