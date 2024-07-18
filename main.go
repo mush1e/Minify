7cbb74181156ae25e42ae728d69ae4e900406a8a
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2024-07-18 01:41:32 -0400

    task/ control structures

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+func division(numerator, denominator int) (result, remainder int, err error) {
+	if denominator == 0 {
+		err = errors.New("Cannot divide by 0")
+		return 0, 0, err
+	}
+	result = numerator / denominator
+	remainder = numerator % denominator
+	return
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	defer fmt.Println("Hello world!")
+	result, remainder, err := division(12, 3)
+	if err != nil {
+		fmt.Println(err)
+	} else if remainder == 0 {
+		fmt.Println("Perfect division")
+	} else {
+		fmt.Println(result, remainder, err)
+	}
 }
